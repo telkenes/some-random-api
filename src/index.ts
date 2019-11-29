90caa9254e170a94787c5bc3c2cae7d035ef534f
GamesProSeif <seifmansour2014@outlook.com>
2019-11-29 15:19:26 +0300

    Add util function

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,4 @@ export * from './structures/ImagesCategory';
 export * from './structures/OtherCategory';
 export * from './structures/PokemonCategory';
 export * from './structures/WelcomeCategory';
+export * from './util';
